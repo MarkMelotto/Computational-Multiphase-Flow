240d69eac151caf045cbb351abe781a3543a0473
MarkMelotto <markmelotto@gmail.com>
2023-03-16 11:44:27 +0100

    cleaned the code

--- Project_CMF.py
+++ Project_CMF.py
@@ -30,37 +30,19 @@ def initialize(ny, nx):
     A[-1, -1] = -3
     A[0, 0] = -3
 
-    b = Delta_P * (dy) ** 2 / (mu) * np.ones(ny)
+    # Def b in Ax=b, this i a like source term
+    b = (Delta_P * dy**2 / mu) * np.ones(ny)
 
     return A, b, x, y
-
-# N-S Eq for steady-state fully developed flow: mu * d^2/dy^2 u_x = dP/dx
-# the continuity Eq gives: du_x/dx = 0
-
-#Def b in Ax=b, this i a like source term
-# b = Delta_P*(dy)**2/(mu)*np.ones(ny)
-# b[0] = -10
-#b[-1]=0
 
 def boundary_conditions(b, bc_left=0, bc_right=0):
     b[0] = -bc_left
     b[-1] = bc_right
     return b
 
-
-# Def matrix to solve, this is the A in Ax=b
-# Matrix = np.zeros([ny,ny])
-# Matrix[np.arange(ny),np.arange(ny)]=-2
-# Matrix[np.arange(ny-1),np.arange(ny-1)+1] = 1
-# Matrix[np.arange(ny-1)+1,np.arange(ny-1)] = 1
-# Matrix[-1,-1] = -3
-# Matrix[0,0] = -3
-
-#invert matrix
-# M_inv = np.linalg.inv(Matrix)
-# U_num = np.dot(b,M_inv)
-
 def solve(A,b):
+    # N-S Eq for steady-state fully developed flow: mu * d^2/dy^2 u_x = dP/dx
+    # the continuity Eq gives: du_x/dx = 0
     M_inv = np.linalg.inv(A)
     U_numerical = np.dot(b, M_inv)
     return U_numerical
@@ -68,17 +50,10 @@ def solve(A,b):
 #analytical solution to pipe flow
 # U_an = -Delta_P /(2*mu) *y*(H-y)
 
-#Cal the error and RMS
-# Error = (U_num - U_an)
-# RMS = np.sqrt(np.sum(Error**2)/len(Error))
-# print('RMS=',RMS)
-
 def error_RMS(numerical_solution, analytical_solution):
     error = (numerical_solution-analytical_solution)
     rms = np.sqrt(np.sum(error ** 2) / len(error))
     return rms
-
-#print('Error=',Error)
 
 #plot Num and Ana solutions
 # plt.errorbar(y,U_num, Error, fmt ='*', label = 'Numerical Solution')
@@ -96,15 +71,8 @@ def plot_flow(y, solution):
     plt.grid()
     plt.show()
 
-#for fun, plot the velocity profile in x-y direction
-# u = U_num*np.ones([nx,ny])
-# plt.contourf(u,ny)
-# plt.xlabel('channel height')
-# plt.ylabel('channel length')
-# plt.colorbar()
-# plt.show()
-
 def plot_flow_contour(solution, nx,ny):
+    # for fun, plot the velocity profile in x-y direction
     u = solution * np.ones([nx, ny])
     plt.contourf(u, ny)
     plt.title("Velocity field contour")
