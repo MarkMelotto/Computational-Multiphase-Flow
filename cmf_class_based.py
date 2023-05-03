82bcd416c22415011083adebb5188fdbd9583c68
Mark Melotto <M.Melotto@student.tudelft.nl>
2023-05-03 13:34:22 +0200

    basic no flow at the wall boundary condition is done

--- cmf_class_based.py
+++ cmf_class_based.py
@@ -1,10 +1,12 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 class Grid:
 
     def __init__(self, rho, kinematic_viscosity):
         self.rho = rho
         self.kinematic_viscosity = kinematic_viscosity
+        self.dynamic_viscosity = kinematic_viscosity*rho
         self.grid = None
         self.number_of_nodes = None
 
@@ -15,6 +17,7 @@ class Grid:
         gridpoints = np.zeros((), dtype=[
         ("velocity", float, nodes),
         ("position", float, nodes),
+        ("analytical_velocity", float, nodes),
         ])
         for i,position in enumerate(position_array):
             gridpoints["velocity"][i] = initial_velocity
@@ -22,20 +25,48 @@ class Grid:
 
         self.grid = gridpoints
 
-    def basic_velocity_CDM(self, bc_left, bc_right):
-        A = -2*np.eye((self.number_of_nodes,self.number_of_nodes))\
-            + np.eye((self.number_of_nodes,self.number_of_nodes), k=1) \
-            + np.eye((self.number_of_nodes,self.number_of_nodes), k=-1)
-        print(A)
-        A[0,0] = bc_left
-        A[-1,-1] = bc_right
-        print(A)
+    def basic_velocity_CDM(self, bc_left, bc_right, pressure_gradient):
+        A = -2*np.eye(self.number_of_nodes,self.number_of_nodes)\
+            + np.eye(self.number_of_nodes,self.number_of_nodes, k=1) \
+            + np.eye(self.number_of_nodes,self.number_of_nodes, k=-1)
+
+        A /= length/self.number_of_nodes
+        # A[0,0] = -3
+        # A[-1,-1] = -3
+
+        b = 2*np.ones(self.number_of_nodes)*pressure_gradient/(self.dynamic_viscosity * self.rho)
+        b[0] = bc_left
+        b[-1] = bc_right
+        velocity = np.linalg.solve(A,b)
+        self.grid['velocity'] = velocity
+
+        x = self.get_position()
+        analytical = -(1/self.dynamic_viscosity)*pressure_gradient/2 * (x*(1-x))
+        self.grid["analytical_velocity"] = analytical
 
     def get_velocities(self):
         return self.grid["velocity"]
 
+    def get_analytical_solution(self):
+        return self.grid["analytical_velocity"]
+
     def get_position(self):
         return self.grid["position"]
+
+    def plot(self, analyicalTF):
+        velocity = self.get_velocities()
+        position = self.get_position()
+        analytical = self.get_analytical_solution()
+        plt.plot(position, velocity, label="numerical solution")
+        if analyicalTF:
+            plt.plot(position, analytical, label="analytical solution", ls='--')
+            plt.legend()
+        plt.xlabel("x")
+        plt.ylabel("velocity")
+        plt.grid()
+        plt.show()
+
+
 
 
 
@@ -43,19 +74,17 @@ class Grid:
 if __name__ == "__main__":
     rho = 1000  # water
     kinematic_viscosity = 0.00105  # water
+    pressure_gradient = -1
 
     # Make our initial boy
     grid = Grid(rho, kinematic_viscosity)
 
     length = 1
-    nodes = 5
+    nodes = 500
     initial_velocity = 1
     # bc_velocity = 0
     grid.make_grid(length, nodes, initial_velocity)
     # |--------do simulations--------|
-    grid.basic_velocity_CDM(-3,-3)
-
-
-    # |--------do plots--------|
-    x = grid.get_position()
-    velocities = grid.get_velocities()
+    """ first exercise wall boundary"""
+    grid.basic_velocity_CDM(0,0, pressure_gradient)
+    grid.plot(True)
