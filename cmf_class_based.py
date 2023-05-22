ad07a0ddf2f3c5970e2029c83b67f9a23f5a6c06
Mark Boots <markboots101@gmail.com>
2023-05-22 23:56:46 +0200

    Eddy viscocity model, moet nog wel geverivieerd worden

--- cmf_class_based.py
+++ cmf_class_based.py
@@ -6,7 +6,7 @@ class Grid:
     def __init__(self, rho, kinematic_viscosity):
         self.rho = rho
         self.kinematic_viscosity = kinematic_viscosity
-        self.dynamic_viscosity = kinematic_viscosity*rho
+        self.dynamic_viscosity = kinematic_viscosity * rho
         self.grid = None
         self.number_of_nodes = None
         self.boundary_conditions = {"bc_left": 0, "bc_right": 0}
@@ -15,14 +15,16 @@ class Grid:
     def make_grid(self, length, nodes, initial_velocity):
         self.number_of_nodes = nodes
         self.length = length
-        boundary_node_position = (length/nodes)/2
-        position_array = np.linspace(boundary_node_position,length-boundary_node_position, nodes)
+        self.dx = length / nodes
+        boundary_node_position = length / nodes / 2
+
+        position_array = np.linspace(boundary_node_position, length - boundary_node_position, nodes)
         gridpoints = np.zeros((), dtype=[
                             ("velocity", float, nodes),
                             ("position", float, nodes),
                             ("analytical_velocity", float, nodes),
                             ])
-        for i,position in enumerate(position_array):
+        for i, position in enumerate(position_array):
             gridpoints["velocity"][i] = initial_velocity
             gridpoints["position"][i] = position
 
@@ -30,24 +32,26 @@ class Grid:
 
     def analytical_sol_1d_pressure(self, x, bc_left, bc_right, pressure_gradient):
         c2 = bc_left
-        c1 = (self.dynamic_viscosity/self.length)*bc_right - self.length*pressure_gradient/2 - self.dynamic_viscosity*bc_left/self.length
-        return pressure_gradient*x**2/(2*self.dynamic_viscosity) + c1*x/self.dynamic_viscosity + c2
-    def basic_velocity_CDM(self, bc_left, bc_right, pressure_gradient):
-        A = -2*np.eye(self.number_of_nodes,self.number_of_nodes)\
-            + np.eye(self.number_of_nodes,self.number_of_nodes, k=1) \
-            + np.eye(self.number_of_nodes,self.number_of_nodes, k=-1)
-        dx = length/self.number_of_nodes
+        c1 = (self.dynamic_viscosity / self.length) * bc_right -\
+              self.length*pressure_gradient / 2 - self.dynamic_viscosity * bc_left / self.length
+        return pressure_gradient * x**2 / (2 * self.dynamic_viscosity) + c1 * x / self.dynamic_viscosity + c2
+    
+    def laminar_velocity(self, bc_left, bc_right, pressure_gradient):
+        """Calculates the laminar velocity with the Central difference method."""
+        A = -2 * np.eye(self.number_of_nodes) \
+            + np.eye(self.number_of_nodes, k=1) \
+            + np.eye(self.number_of_nodes, k=-1)
+        # dx = length / self.number_of_nodes
         # A /= dx
-        A[0,0] = -3
-        A[-1,-1] = -3
+        A[0, 0] = -3
+        A[-1, -1] = -3
 
-        b = np.ones(self.number_of_nodes) * pressure_gradient * dx**2 / self.dynamic_viscosity
-        # print(b)
-        b[0] -= 2*bc_left
+        b = np.ones(self.number_of_nodes) * pressure_gradient * self.dx**2 / self.dynamic_viscosity
+        b[0] -= 2 * bc_left
         # b[1] -= gradient
-        b[-1] -= 2*bc_right
+        b[-1] -= 2 * bc_right
         # print(b)
-        velocity = np.linalg.solve(A,b)
+        velocity = np.linalg.solve(A, b)
         self.grid['velocity'] = velocity
 
         x = self.get_position()
@@ -56,6 +60,50 @@ class Grid:
         self.boundary_conditions["bc_left"] = bc_left
         self.boundary_conditions["bc_right"] = bc_right
 
+
+    def turbulent_velocity(self, bc_left, bc_right, pressure_gradient):
+        A = np.zeros([self.number_of_nodes, self.number_of_nodes])
+        for j in range(self.number_of_nodes):
+            for i in range(self.number_of_nodes):
+                if i == j:
+                    A[i, j] = -2 * self.dynamic_viscosity - self.rho * \
+                        (self.eddy_viscosity(i) + self.eddy_viscosity(i + 1))
+                elif (i + 1) == j:
+                    A[i, j] = self.dynamic_viscosity + self.rho * self.eddy_viscosity(i)
+                elif (i - 1) == j:
+                    A[i, j] = self.dynamic_viscosity + self.rho * self.eddy_viscosity(i + 1)
+
+# TODO: Kan nog fout zijn, metname A[-1, -1] moet gecontrolleerd worden
+        A[0, 0] = -3 * self.dynamic_viscosity - self.rho * (self.eddy_viscosity(0) + self.eddy_viscosity(1))
+        A[-1, -1] = -3 * self.dynamic_viscosity - self.rho * \
+            (self.eddy_viscosity(self.number_of_nodes - 1) + self.eddy_viscosity(self.number_of_nodes - 2))
+
+        b = np.ones(self.number_of_nodes) * pressure_gradient * self.dx**2
+        b[0] -= 2 * bc_left
+        b[-1] -= 2 * bc_right
+
+        velocity = np.linalg.solve(A, b)
+        self.grid['velocity'] = velocity
+
+    def eddy_viscosity(self, i):
+        kappa = 0.41
+        kappa0 = 0.09
+        boundary_layer = 0.1 * self.length
+        pre_term = (kappa * boundary_layer)**2
+        
+        # TODO: if statement moet nog worden aangepast met juiste ghost cell check en return (weet niet 100% zeker of dit klopt)
+        if (i >= self.number_of_nodes) or (i <= 1):
+            return 0
+        else:
+            velocity_difference = np.abs(self.grid["velocity"][i] - self.grid["velocity"][i - 1]) / self.dx
+
+            if (i - 1) * self.dx < boundary_layer:
+                return pre_term * ((i - 1) * self.dx)**2 * velocity_difference
+            elif (i - 1) * self.dx > (1 - boundary_layer):
+                return pre_term * ((self.number_of_nodes - i + 1) * self.dx)**2 * velocity_difference
+            
+            return pre_term * velocity_difference * self.dx**2
+    
     def get_velocities(self):
         return self.grid["velocity"]
 
@@ -69,19 +117,18 @@ class Grid:
         velocity = self.get_velocities()
         position = self.get_position()
         analytical = self.get_analytical_solution()
+
         plt.plot(position, velocity, label="numerical solution")
+
         if analyicalTF:
             plt.plot(position, analytical, label="analytical solution", ls='--')
             plt.title(f"bc_left = {self.boundary_conditions['bc_left']:.1f} m/s, bc_right = {self.boundary_conditions['bc_right']:.1f} m/s")
             plt.legend()
+
         plt.xlabel("x")
         plt.ylabel("velocity")
         plt.grid()
         plt.show()
-
-
-
-
 
 
 if __name__ == "__main__":
@@ -106,5 +153,7 @@ if __name__ == "__main__":
     # |--------do simulations--------|
     """ first exercise wall boundary"""
 
-    grid.basic_velocity_CDM(bc_left=3,bc_right=2, pressure_gradient=pressure_gradient)
-    grid.plot(True)
+    grid.laminar_velocity(bc_left=3,bc_right=2, pressure_gradient=pressure_gradient)
+    grid.plot(False)
+    grid.turbulent_velocity(bc_left=3,bc_right=3, pressure_gradient=pressure_gradient)
+    grid.plot(False)
