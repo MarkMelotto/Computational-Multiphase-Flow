2c0d87486848468dc4eb60be13aacd8ef0ffebab
Mark Melotto <M.Melotto@student.tudelft.nl>
2023-07-15 17:34:53 +0200

    removed old functions

--- multiphase_functions.py
+++ multiphase_functions.py
@@ -1,11 +1,5 @@
 import numpy as np
 
-def calc_T_t(U, V, l, dx):
-    du_dy = calc_U_prime(U, V, l, dx)
-    T_t = np.zeros_like(du_dy)
-    T_t[1:-1, :] = 1/du_dy[1:-1, :]
-    return T_t
-
 def calc_T_t_new(U,  l, dx):
     du_dy = calc_U_prime_new(U, l, dx)
     T_t = np.zeros_like(du_dy)
@@ -13,55 +7,24 @@ def calc_T_t_new(U,  l, dx):
     T_t[np.isnan(T_t)] = 0
     return T_t
 
-def calc_U_prime(U, V, l, dx):
-    du_dy = calc_dU_dy(U, V, dx)
-    return l*du_dy
 
 def calc_U_prime_new(U, l, dx):
     du_dy = calc_dU_d_new(U, dx)
     return l*du_dy
 
-def calc_dU_dy(U, V, dx):
-    # du_dy = np.zeros((U.shape[0],V.shape[1]))
-    # du_dx = np.zeros((U.shape[0],V.shape[1]))
-    # dv_dy = np.zeros((U.shape[0],V.shape[1]))
-    # dv_dx = np.zeros((U.shape[0],V.shape[1]))
-    # du_dy = (U[2:-1, 1:-1] - U[1:-2, 1:-1]) / dx
-
-    du_dx = (U[1:-1, 2:-1] - U[1:-1, 1:-2]) / dx
-
-    dv_dy = (V[2:-1, 1:-1] - V[1:-2, 1:-1]) / dx
-    # dv_dx = (V[1:-1, 2:-1] - V[1:-1, 1:-2]) / dx
-
-    # u_prime = np.sqrt(du_dx*du_dx + dv_dy*dv_dy)/4
-    u_prime = np.sqrt(np.mean(du_dx)**2+np.mean(dv_dy)**2)
-    return u_prime
 
 def calc_dU_d_new(U, dx):
     du_dy = np.zeros_like(U)
     du_dx = np.zeros_like(U)
-    # dv_dy = np.zeros((U.shape[0],V.shape[1]))
-    # dv_dx = np.zeros((U.shape[0],V.shape[1]))
-    # print(f"u shape = {U.shape}")
+
     du_dy[1:-1, 1:-1] = (U[2:, 1:-1] - U[:-2, 1:-1]) / dx
-    # du_dy[np.isnan(du_dy)] = 0
-    # print(f"du_dy shape = {du_dy.shape}")
+
     du_dx[1:-1, 1:-1] = (U[1:-1, 2:] - U[1:-1, :-2]) / dx
-    # du_dx[np.isnan(du_dx)] = 0
-    # print(f"du_dx shape = {du_dx.shape}")
-    # dv_dy = (V[2:-1, 1:-1] - V[1:-2, 1:-1]) / dx
-    # dv_dx = (V[1:-1, 2:-1] - V[1:-1, 1:-2]) / dx
+
 
     u_prime = np.sqrt((du_dx*du_dx + du_dy*du_dy + 2*du_dx*du_dy)/4)
     u_prime[np.isnan(u_prime)] = 0
-    # u_prime = np.sqrt(np.mean(du_dx)**2+np.mean(dv_dy)**2)
     return u_prime
-
-
-def calc_U_2i_U_2j(T_t, T_p, U, V, l, dx):
-    U_1i_U_1j = calc_U_prime(U, V, l, dx)**2
-
-    return (T_t/(T_p+T_t))*U_1i_U_1j
 
 def calc_U_2i_U_2j_new(T_t, T_p, U, l, dx):
     # print(f"l is {l}")
@@ -71,27 +34,17 @@ def calc_U_2i_U_2j_new(T_t, T_p, U, l, dx):
 
 def get_F_i(nu_f, D_p, rho_2, a_2, U2mean, U1mean):
     U_2i_min_U_1i = U2mean - U1mean
-    # print(f'U2 min U1 = {U_2i_min_U_1i}')
     interfacial_stress = 18*nu_f * rho_2 * a_2 * U_2i_min_U_1i / D_p**2
-    # interfacial_stress = 18*nu_f * rho_2 * a_2 * U_2i_min_U_1i
 
     return interfacial_stress
 
 def get_F_i_fast(nu_f, D_p, rho_2, a_2, U2mean, U1mean):
     U_2i_min_U_1i = U2mean - U1mean
-    # print(f'U2 min U1 = {U_2i_min_U_1i}')
-    # interfacial_stress = 18*nu_f * rho_2 * a_2 * U_2i_min_U_1i / D_p**2
-    # print(f"a2 {a_2.shape}")
-    # print(f"U2-U1 {U_2i_min_U_1i.shape}")
     interfacial_stress = 18*nu_f * rho_2 * a_2 * U_2i_min_U_1i
 
     return interfacial_stress
 def get_F_i_fast_concentration(nu_f, D_p, rho_2, a_2, U2mean, U1mean):
     U_2i_min_U_1i = U2mean - U1mean
-    # print(f'U2 min U1 = {U_2i_min_U_1i}')
-    # interfacial_stress = 18*nu_f * rho_2 * a_2 * U_2i_min_U_1i / D_p**2
-    # print(f"a2 {a_2.shape}")
-    # print(f"U2-U1 {U_2i_min_U_1i.shape}")
     interfacial_stress = 18*nu_f * rho_2 * a_2[1:-1, 1:-1] * U_2i_min_U_1i
 
     return interfacial_stress
@@ -186,8 +139,8 @@ if __name__ == "__main__":
     l_x = np.zeros((Ny + 1, Nx))
     l_x[:, :] = f_l_x[:, np.newaxis]
 
-    u_prime = calc_dU_dy(u_prev, v_prev, dx)
-    print(u_prime)
+    # u_prime = calc_dU_dy(u_prev, v_prev, dx)
+    # print(u_prime)
 
     uprime = calc_dU_d_new(u_prev, dx)
     print(uprime)
